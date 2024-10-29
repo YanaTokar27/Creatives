689a562b427f4f7202889227955971dc6366cc90
yanatokar <yanochka.tokar22@gmail.com>
2024-10-29 22:38:23 +0200

    Fixed review comments

--- Creative-4/script.js
+++ Creative-4/script.js
@@ -1,43 +1,46 @@
-setTimeout(() => {
-  const firstSlide = document.querySelector(".slide-first");
-  firstSlide.parentElement.removeChild(firstSlide);
+const firstSlide = document.querySelector(".slide-first");
+firstSlide.addEventListener("animationend", (event) => {
+  const delayAfterAnimation = 500;
+  setTimeout(() => {
+    firstSlide.parentElement.removeChild(firstSlide);
 
-  const hidden = document.querySelector(".hidden-woman");
-  hidden.classList.remove("hidden-woman");
+    const hidden = document.querySelector(".hidden-woman");
+    hidden.classList.remove("hidden-woman");
 
-  const sliderForward = document.querySelector(".slider-forward");
-  sliderForward.classList.remove("slider-forward");
-  sliderForward.classList.add("slider-forward-back");
+    const sliderForward = document.querySelector(".slider-forward");
+    sliderForward.classList.remove("slider-forward");
+    sliderForward.classList.add("slider-forward-back");
 
-  let pauseTranslateX = 0;
+    let pauseTranslateX = 0;
 
-  const slider = document.querySelector(".slider");
+    const slider = document.querySelector(".slider");
 
-  slider.addEventListener("touchstart", (e) => {
-    startX = e.touches[0].clientX;
-    sliderForward.classList.add("pause-animation");
+    slider.addEventListener("touchstart", (e) => {
+      startX = e.touches[0].clientX;
+      sliderForward.classList.add("pause-animation");
 
-    const computedStyle = window.getComputedStyle(sliderForward);
-    pauseTranslateX = parseInt(computedStyle.transform.split(",")[4]);
-  });
+      const computedStyle = window.getComputedStyle(sliderForward);
+      pauseTranslateX = parseInt(computedStyle.transform.split(",")[4]);
+    });
 
-  let prevX = 0;
-  let newX = 0;
+    let prevX = 0;
+    let newX = 0;
 
-  slider.addEventListener("touchmove", (e) => {
-    const endX = e.touches[0].clientX;
-    const diffX = endX - startX;
-    newX = prevX + diffX;
-    newX = Math.min(newX, -pauseTranslateX); // left
-    const minAnimationTranslateX = -1000;
-    newX = Math.max(newX, minAnimationTranslateX - pauseTranslateX); // right
-    slider.style.translate = `${newX}px 0px`;
-  });
+    slider.addEventListener("touchmove", (e) => {
+      const endX = e.touches[0].clientX;
+      const diffX = endX - startX;
+      newX = prevX + diffX;
+      newX = Math.min(newX, -pauseTranslateX); // left
+      const minAnimationTranslateX = -1000;
+      newX = Math.max(newX, minAnimationTranslateX - pauseTranslateX); // right
+      slider.style.translate = `${newX}px 0px`;
+    });
 
-  slider.addEventListener("touchend", (e) => {
-    prevX = newX;
-  });
-}, 4000);
+    slider.addEventListener("touchend", (e) => {
+      prevX = newX;
+    });
+  }, delayAfterAnimation);
+});
 
 const video = document.getElementById("myVideo");
 const screenshot = document.getElementById("screenshot");
@@ -45,7 +48,7 @@ const screenshot = document.getElementById("screenshot");
 video.addEventListener("ended", function () {
   video.classList.add("hidden");
 
-  // Відображаємо скріншот
+  // Display screenshot
   screenshot.classList.remove("hidden");
 });
 
@@ -53,22 +56,18 @@ screenshot.addEventListener("click", function () {
   screenshot.classList.add("hidden");
 
   video.classList.remove("hidden");
-  video.currentTime = 0; // Починаємо від початку
-  video.play(); // Запускаємо відтворення
+  video.currentTime = 0; // Start from the begining
+  video.play(); // Start to play
 });
 
-const muteBtn = document.querySelector(".muteBtn");
-const soundBtn = document.querySelector(".soundBtn");
+const muteBtn = document.querySelector(".mute-btn");
 
 muteBtn.addEventListener("click", function () {
-  video.muted = false;
-  muteBtn.classList.add("hidden");
-  soundBtn.classList.remove("hidden");
-});
-
-// Обробник для кнопки Sound
-soundBtn.addEventListener("click", function () {
-  video.muted = true;
-  soundBtn.classList.add("hidden");
-  muteBtn.classList.remove("hidden");
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.src = "./Assets/sound.png";
+  } else {
+    video.muted = true;
+    muteBtn.src = "./Assets/mute.png";
+  }
 });
