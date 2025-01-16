0201076551f98bae78aa05fd891885ea872d03bc
yanatokar <yanochka.tokar22@gmail.com>
2025-01-16 13:03:12 +0200

    Fixed issues

--- Lottery/script.js
+++ Lottery/script.js
@@ -1,20 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const appContainer = document.querySelector(".container");
-  appContainer.classList.add("start-animation");
+document.addEventListener('DOMContentLoaded', () => {
+  const appContainer = document.querySelector('.container');
+  appContainer.classList.add('start-animation');
 });
 
 const snowContainer = document.querySelector('.snow-container');
 const numberOfSnowflakes = 30;
+const MIN_SIZE = 0.3;
+const MAX_SIZE = 2;
+const MIN_DURATION = 10;
+const MAX_DURATION = 3;
+const DELAY = 10;
 
 for (let i = 0; i < numberOfSnowflakes; i++) {
     const snowflake = document.createElement('div');
     snowflake.classList.add('snowflake');
     
     // Випадкове розташування та тривалість (random - діапазон [0 до 1])
-    const size = Math.random() * 2 + 0.3; // Розмір від 0.3 до 2.3
+    const size = Math.random() * MAX_SIZE + MIN_SIZE; // Розмір від 0.3 до 2.3
     const leftPosition = Math.random() * 100; // Випадкове розташування
-    const animationDuration = Math.random() * 3 + 10; // Тривалість від 10 до 13 секунд
-    const animationDelay = Math.random() * 10; // Затримка до 10 секунд
+    const animationDuration = Math.random() * MAX_DURATION + MIN_DURATION; // Тривалість від 10 до 13 секунд
+    const animationDelay = Math.random() * DELAY; // Затримка до 10 секунд
 
     // Додаємо стилі для кожної сніжинки
     snowflake.style.width = `${size}vw`;
