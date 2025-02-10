5671559c2c2cab3907f313c3634820dfb4ee6d6c
yanatokar <yanochka.tokar22@gmail.com>
2025-02-10 11:20:43 +0200

    Foodpanda-first commit

---
+++ Foodpanda-431/script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const appContainer = document.querySelector(".LM_container");
+    appContainer.classList.add("animation");
+  });
+
+const video = document.getElementById("myVideo");  
+
+video.addEventListener("ended", () => {
+    video.classList.add("hidden");
+    btn.classList.add("hidden");
+    screenshot.classList.remove("hidden");
+});
+
+const screenshot = document.getElementById("screenshot");
+
+screenshot.addEventListener("click", () => {
+    btn.classList.remove("hidden");
+    screenshot.classList.add("hidden");
+  
+    video.classList.remove("hidden");
+    video.currentTime = 0;
+    video.play(); 
+});
+  
+const btn = document.querySelector(".btn");
+  
+btn.addEventListener("click", () => {
+    video.muted = !video.muted;
+    btn.classList.toggle("sound-btn");
+});
