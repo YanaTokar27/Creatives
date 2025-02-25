64813487be41622bdc817520ba026768111a455b
yanatokar <yanochka.tokar22@gmail.com>
2025-02-25 13:39:05 +0200

    Video on landscape orientation

--- Foodpanda-431/index.js
+++ Foodpanda-431/index.js
@@ -12,6 +12,23 @@ document.addEventListener("DOMContentLoaded", () => {
     screenshot.classList.remove("hidden");
   });
 
+  function changeOrientation() {
+    const landscapeBlock = document.querySelector(".LM_landscape");
+  
+    if (window.matchMedia("(orientation: landscape)").matches) {
+      video.pause();
+      landscapeBlock.style.display = "block";
+    } else {
+      video.play();
+      landscapeBlock.style.display = "none";
+    }
+  }
+  
+  window.addEventListener("orientationchange", changeOrientation);
+  window.addEventListener("resize", changeOrientation);
+  
+  changeOrientation();
+
   const screenshot = document.getElementById("screenshot");
 
   screenshot.addEventListener("click", () => {
